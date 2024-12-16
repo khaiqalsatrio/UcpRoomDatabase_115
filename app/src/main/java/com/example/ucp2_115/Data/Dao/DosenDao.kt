9fda203337d07225b28070c8b922f7a44dc51ace
Khaiqol <Khaiqol@gmail.com>
2024-12-16 17:15:41 +0700

    Membuat Select from dosen by nama dan select from dosen where nidn

--- app/src/main/java/com/example/ucp2_115/Data/Dao/DosenDao.kt
+++ app/src/main/java/com/example/ucp2_115/Data/Dao/DosenDao.kt
@@ -1,8 +1,12 @@
 package com.example.ucp2_115.Data.Dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.ucp2_115.Data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface DosenDao {
@@ -11,5 +15,12 @@ interface DosenDao {
     suspend fun insertDosen(
         dosen: Dosen
     )
+
+    @Query("SELECT * FROM dosen ORDER BY nama ASC")
+    fun getAllDosen(): Flow<List<Dosen>>
+
+    @Query("SELECT * FROM dosen WHERE nidn = :nidn")
+    fun getDosen(nidn: String): Flow<Dosen>
+
 
 }
