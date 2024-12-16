8595fa504285a34a9a4738b58cc22f042f435526
Khaiqol <Khaiqol@gmail.com>
2024-12-16 17:24:50 +0700

    Membuat Delete dan update matakuliah di dao

--- app/src/main/java/com/example/ucp2_115/Data/Dao/MatakuliahDao.kt
+++ app/src/main/java/com/example/ucp2_115/Data/Dao/MatakuliahDao.kt
@@ -1,8 +1,10 @@
 package com.example.ucp2_115.Data.Dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.ucp2_115.Data.entity.MataKuliah
 import kotlinx.coroutines.flow.Flow
 
@@ -19,4 +21,10 @@ interface MatakuliahDao {
 
     @Query("SELECT * FROM matakuliah WHERE kode = :kode")
     fun getMataKuliah(kode: String): Flow<MataKuliah>
+
+    @Delete
+    suspend fun deleteMataKuliah(mataKuliah: MataKuliah)
+
+    @Update
+    suspend fun updateMataKuliah(mataKuliah: MataKuliah)
 }
