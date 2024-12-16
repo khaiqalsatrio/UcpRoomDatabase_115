25eccf7cc358e005ebcc53ee4f2e3f5ffd4439cd
Khaiqol <Khaiqol@gmail.com>
2024-12-16 17:23:28 +0700

    Membuat select from matakuliah by nama dan Where kode

---
+++ app/src/main/java/com/example/ucp2_115/Data/Dao/MatakuliahDao.kt
@@ -0,0 +1,22 @@
+package com.example.ucp2_115.Data.Dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.ucp2_115.Data.entity.MataKuliah
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MatakuliahDao {
+
+    @Insert
+    suspend fun insertMatakuliah(
+        mataKuliah: MataKuliah
+    )
+
+    @Query("SELECT * FROM matakuliah ORDER BY nama ASC")
+    fun getAllMataKuliah(): Flow<List<MataKuliah>>
+
+    @Query("SELECT * FROM matakuliah WHERE kode = :kode")
+    fun getMataKuliah(kode: String): Flow<MataKuliah>
+}
