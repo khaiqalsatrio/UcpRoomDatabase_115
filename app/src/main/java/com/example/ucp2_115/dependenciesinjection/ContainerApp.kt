323434854b042960c8f241ad8cbfe816e95f006e
Khaiqol <Khaiqol@gmail.com>
2024-12-21 10:41:55 +0700

    memperbaiki container app dan membuat detail dosen view

--- app/src/main/java/com/example/ucp2_115/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2_115/dependenciesinjection/ContainerApp.kt
@@ -2,6 +2,10 @@ package com.example.ucp2_115.dependenciesinjection
 
 import android.content.Context
 import com.example.ucp2_115.Data.Database.KrsDatabase
+import com.example.ucp2_115.repository.LocalRepositoryDosen
+import com.example.ucp2_115.repository.LocalRepositoryMatakuliah
+import com.example.ucp2_115.repository.RepositoryDosen
+import com.example.ucp2_115.repository.RepositoryMatakuliah
 
 // Interface untuk Dependency Injection
 interface InterfaceContainerApp {
